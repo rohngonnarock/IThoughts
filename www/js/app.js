d4d15a3fe8ff156c50de185a2298330128632472
Rahul Prajapati <rohngonnarock@gmail.com>
2015-08-01 01:51:44 +0530

    Next Previous issue resolve 1

--- www/js/app.js
+++ www/js/app.js
@@ -74,52 +74,13 @@ Iquotes.config(function ($stateProvider, $urlRouterProvider) {
 });
 
 
-Iquotes.config(function ($stateProvider, $urlRouterProvider) {
-    $stateProvider
-
-      .state('app', {
-          url: '/app',
-          abstract: true,
-          templateUrl: 'templates/menu.html',
-          controller: 'AppCtrl'
-      })
-
-    .state('app.search', {
-        url: '/search',
-        views: {
-            'menuContent': {
-                templateUrl: 'templates/search.html'
-            }
+Iquotes.factory('photosFactory', function ($http) {
+    return{
+        getPhotos : function(n,c) {
+            return $http({
+                url: 'http://ithoughs.azurewebsites.net/api/quote?applicationId=2&currentQuoteid='+c+'&quotetype=' + n,
+                method: 'GET'
+            })
         }
-    })
-
-    .state('app.browse', {
-        url: '/browse',
-        views: {
-            'menuContent': {
-                templateUrl: 'templates/browse.html'
-            }
-        }
-    })
-      .state('app.playlists', {
-          url: '/playlists',
-          views: {
-              'menuContent': {
-                  templateUrl: 'templates/playlists.html',
-                  controller: 'PlaylistsCtrl'
-              }
-          }
-      })
-
-    .state('app.single', {
-        url: '/playlists/:playlistId',
-        views: {
-            'menuContent': {
-                templateUrl: 'templates/playlist.html',
-                controller: 'PlaylistCtrl'
-            }
-        }
-    });
-    // if none of the above states are matched, use this as the fallback
-    $urlRouterProvider.otherwise('/app/playlists');
-});
+    }
+})
